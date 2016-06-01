d13c8177e7470f789c30ea12c240a65db3dad008
IgorNeb <globaltelnet7.62@gmail.com>
2016-06-01 17:13:01 +0300

    second

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -1,4 +1,25 @@
 package Controller;
 
-public class Controller {
+import Model.Main;
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+    public void goToWindow2(ActionEvent event) {
+        try {
+           new departure();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/Model/Main.java
+++ src/Model/Main.java
@@ -4,16 +4,28 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
+
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("/View/PrimaryWindow.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+    public void start(Stage stage) throws Exception{
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/View/PrimaryWindow.fxml"));
+        BorderPane load = (BorderPane) loader.load();
+
+        Scene scene = new Scene(load);
+        stage.setScene(scene);
+        stage.setTitle("Window 1");
+        stage.show();
+    }
+
+    public void departure() throws Exception
+    {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/View/DepartureWindow.fxml"));
+        AnchorPane load = (AnchorPane) loader.load();
+
     }
 
 
